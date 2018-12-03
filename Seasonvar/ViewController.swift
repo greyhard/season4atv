970ac370e226a98723cb5867bdcaf86165764ffd
greyhard <greyhard@gmail.com>
2018-12-03 23:10:16 +0300

    Initial Commit

---
+++ Seasonvar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Seasonvar
+//
+//  Created by Denis Ilynikh on 03/12/2018.
+//  Copyright © 2018 Denis Ilynikh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
